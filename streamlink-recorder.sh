ddbb65007ecf7bb21efc04e76f15dc7bddcdb09e
Adriel Kloppenburg <adriel@users.noreply.github.com>
2022-09-28 02:06:03 +1300

    Cleaned up logic

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -5,28 +5,38 @@ function is_json {
   jq -e . >/dev/null 2>&1 <<<"$1"
 }
 
+# Test if live
+function is_live {
+  test "$(echo "$1" | jq --raw-output '.data[0].type')" == "live"
+}
+
 # Check if API returned message then log if message
 # check_api "twitch api json" "json filter"
 function check_api {
-  channel_info="$1"
+  channel_json="$1"
   filter="$2"
-  if is_json "$channel_info"; then # check input is valid json
-    api_message=$(echo "$channel_info" | jq --raw-output "$filter" 2>&1)
-    if [[ ! -z "$api_message" ]]; then
-      echo "Twitch API message: $api_message"
-      echo "channel_info: $channel_info"
-    fi
-  else
-    if [[ "$channel_info" =~ "Client.Timeout" ]]; then
-      echo "Timeout - couldn't connect to twitch.com - filter: $filter"
-    elif [[ "$channel_info" =~ "no route to host" ]]; then
-      echo "No route to host - couldn't connect to twitch.com - filter: $filter"
-    else
-      echo "Input isn't json."
-      echo "channel_info: $channel_info"
+
+  # Check if token is missing
+  if [[ "$channel_json" =~ "twitch token" || "$channel_json" =~  "Invalid OAuth token" ]]; then
+    echo "Token missing, refreshing it.";
+    twitch token
+    echo "Token refreshed, now waiting for stream to go live."
+  elif is_json "$channel_json"; then
+    filter_response=$(echo "$channel_json" | jq --raw-output "$filter" 2>&1)
+    if [[ "$channel_json" =~ "," && "$filter_response" != "live" ]]; then # if json contains a "," then there is data
+      echo "Twitch returned unexpected JSON data."
       echo "filter: $filter"
+      echo "filter_response: $filter_response"
+      echo "channel_json: $channel_json"
     fi
-    return 255
+  elif [[ "$channel_json" =~ "Client.Timeout" ]]; then
+    echo "Timeout - couldn't connect to twitch.com - filter: $filter"
+  elif [[ "$channel_json" =~ "no route to host" ]]; then
+    echo "No route to host - couldn't connect to twitch.com - filter: $filter"
+  else
+    echo "Unknown response from Twitch."
+    echo "filter: $filter"
+    echo "channel_json: $channel_json"
   fi
 }
 
@@ -36,51 +46,41 @@ twitch configure --client-id "$clientID" --client-secret "$clientSecret"
 echo "Waiting for stream to go live."
 while [[ true ]]; do
 
-  channel_info=$(twitch api get /streams -q "user_login=${streamName}" 2>&1)
+  channel_json=$(twitch api get /streams -q "user_login=${streamName}" 2>&1)
+  check_api "$channel_json" ".data[0].type"
 
-  check_api "$channel_info" ".message" || continue # skip rest of loop if not json
-
-  # Check if token needs refreshing
-  if [[ "$channel_info" =~ "twitch token" || "$channel_info" =~  "Invalid OAuth token" ]]; then
-    echo "Token missing, refreshing it.";
-    twitch token
-    # Try again, now that we have the token, it should work.
-    channel_info=$(twitch api get /streams -q "user_login=${streamName}" 2>&1)
-    echo "Token refreshed, now waiting for stream to go live."
-  fi
-
-  check_api "$channel_info" ".data[0].type" || continue # skip rest of loop if not json
-
-  channel_live=$(echo "$channel_info" | jq --raw-output '.data[0].type')
-  if [[ "$channel_live" == "live" ]]; then
-    echo "$streamName stream is: $channel_live"
+  if is_live "$channel_json"; then
+    echo "$streamName is LIVE!"
 
     date_unix=$(date +'%s')
-    user_name=$(      echo "$channel_info" | jq --raw-output '.data[0].user_name')
-    user_id=$(        echo "$channel_info" | jq --raw-output '.data[0].user_id')
-    started_at=$(     echo "$channel_info" | jq --raw-output '.data[0].started_at')
+    user_name=$(      echo "$channel_json" | jq --raw-output '.data[0].user_name')
+    user_id=$(        echo "$channel_json" | jq --raw-output '.data[0].user_id')
+    started_at=$(     echo "$channel_json" | jq --raw-output '.data[0].started_at')
     started_at_safe=$(echo "$started_at"   | sed -e 's/[^A-Za-z0-9._-]/./g') # safe name for filesystems
-    game_name=$(      echo "$channel_info" | jq --raw-output '.data[0].game_name')
-    stream_title=$(   echo "$channel_info" | jq --raw-output '.data[0].title')
-    viewer_count=$(   echo "$channel_info" | jq --raw-output '.data[0].viewer_count')
+    game_name=$(      echo "$channel_json" | jq --raw-output '.data[0].game_name')
+    title=$(          echo "$channel_json" | jq --raw-output '.data[0].title')
+    viewer_count=$(   echo "$channel_json" | jq --raw-output '.data[0].viewer_count')
 
     echo "user_name: $user_name"
     echo "user_id: $user_id"
     echo "started_at: $started_at"
     echo "started_at_safe: $started_at_safe"
     echo "game_name: $game_name"
-    echo "stream_title: $stream_title"
+    echo "title: $title"
     echo "viewer_count: $viewer_count"
 
+    save_dir="/home/download/${game_name}"
     streamlink "$streamLink" "$streamQuality" "$streamOptions" --stdout | \
       ffmpeg \
       	-hide_banner \
         -loglevel error \
       	-i pipe: \
-        -metadata title="$stream_title" \
+        -metadata title="$title" \
         -metadata album_artist="$user_name" \
         -metadata show="$game_name" \
-        -c copy -movflags faststart "/home/download/${game_name}/${user_name} ${date_unix} ${started_at_safe} ${stream_title} [viewers $viewer_count] (live_dl).mp4"
+        -c copy \
+        -movflags faststart \
+        "${save_dir}/${user_name} ${date_unix} ${started_at_safe} ${title} [viewers $viewer_count] (live_dl).mp4"
   fi
-	sleep 60s
+  sleep 60s
 done
